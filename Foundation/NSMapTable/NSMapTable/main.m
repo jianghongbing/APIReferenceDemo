e522a7b45189453b37caaa661ff2b808ab0f0d82
jianghongbing <jianghongbing1989@qq.com>
2017-06-20 21:40:58 +0800

    add NSMutableIndexSet

---
+++ Foundation/NSMapTable/NSMapTable/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSMapTable
+//
+//  Created by jianghongbing on 2017/6/20.
+//  Copyright © 2017年 jianghongbing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
--- Foundation/NSMutableIndexSet/NSMutableIndexSet/main.m
+++ Foundation/NSMutableIndexSet/NSMutableIndexSet/main.m
@@ -10,8 +10,37 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableIndexSet *indexSet = [NSMutableIndexSet indexSet];
+        //1.添加index
+        [indexSet addIndex:6];
+        [indexSet addIndex:5];
+        [indexSet addIndex:2];
+        NSLog(@"indexSet:%@",indexSet);
+        [indexSet addIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(10, 20)]];
+        NSLog(@"indexSet:%@", indexSet);
+        [indexSet addIndexesInRange:NSMakeRange(100, 10)];
+        NSLog(@"indexSet:%@", indexSet);
+
+        //2.移除index
+        if ([indexSet containsIndex:5]) {
+            [indexSet removeIndex:5];
+        }
+        NSLog(@"indexSet:%@", indexSet);
+
+        [indexSet removeIndexes:[NSIndexSet indexSetWithIndexesInRange:NSMakeRange(20, 10)]];
+        NSLog(@"indexSet:%@", indexSet);
+
+        [indexSet removeIndexesInRange:NSMakeRange(20, 100)];
+        NSLog(@"indexSet:%@", indexSet);
+
+        [indexSet removeAllIndexes];
+        NSLog(@"indexSet:%@", indexSet);
+
+        //3.指定的位置的index开始增加指定的大小
+        [indexSet addIndexesInRange:NSMakeRange(10, 20)];
+        [indexSet shiftIndexesStartingAtIndex:15 by:100];
+        NSLog(@"indexSet:%@", indexSet);
+
     }
     return 0;
 }
