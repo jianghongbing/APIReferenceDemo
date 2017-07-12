7db6656b38d165805667ff7cd77dc9ab461dbc37
jianghongbing <jianghongbing1989@qq.com>
2017-07-12 21:49:07 +0800

    update

---
+++ Foundation/NSDictionary/NSDictionary/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSDictionary
+//
+//  Created by jianghongbing on 2017/7/9.
+//  Copyright © 2017年 jianghongbing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSDictionary *dictionary = [NSDictionary dictionary];
+    }
+    return 0;
+}
