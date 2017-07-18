3c6f09bfeb198cc4ee2733de6563140629c6963c
pantosoft <pantosoft@pantosoftdeMac-mini.local>
2017-07-18 20:17:59 +0800

    add NSDate

--- Foundation/NSDate/NSDate/main.m
+++ Foundation/NSDate/NSDate/main.m
@@ -2,7 +2,7 @@
 //  main.m
 //  NSDate
 //
-//  Created by jianghongbing on 2017/7/9.
+//  Created by ; on 2017/7/9.
 //  Copyright © 2017年 jianghongbing. All rights reserved.
 //
 
@@ -10,9 +10,40 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
-        NSDate
+        //NSDate:用于表示某一时刻的时间
+        //1.获取当前时间
+        NSDate *date = [NSDate date];
+        //2.遥远的过去和将来
+        NSDate *distantFuture = [NSDate distantFuture];
+        NSDate *distantPath = [NSDate distantPast];
+        NSLog(@"date:%@, distantFuture:%@, distantPath:%@", date, distantFuture, distantPath);
+        //3.date的创建
+        NSDate *aDate = [NSDate dateWithTimeIntervalSinceNow:60 * 60 * 24 * 365]; //在当前时间添加多少秒
+        NSLog(@"aDate:%@", aDate);
+        aDate = [NSDate dateWithTimeIntervalSince1970:60 * 60 * 24 * 365 * 20]; //以1970:01:01 00:00:00开始计算
+        NSLog(@"aDate:%@", aDate);
+        aDate = [NSDate dateWithTimeIntervalSinceReferenceDate:60 * 60 * 24 * 365 * 10]; //以2001:01:01 00:00:00开始计算
+        NSLog(@"aDate:%@", aDate);
+        aDate = [date dateByAddingTimeInterval:60 * 60 * 24 * 365]; //在某一个时间的基础上,添加多少秒
+        NSLog(@"aDate:%@", aDate);
+        //4.获取时间戳
+        NSTimeInterval timestamp = aDate.timeIntervalSinceReferenceDate * 1000;
+        NSTimeInterval timestampSinceNow = aDate.timeIntervalSinceNow * 1000;
+        NSTimeInterval timestampSince1970 = aDate.timeIntervalSince1970 * 1000;
+        NSLog(@"timestamp:%f, %f, %f", timestamp, timestampSinceNow, timestampSince1970);
+        
+        //5.NSDate的比较
+        BOOL isEqual = [date isEqualToDate:[NSDate date]];
+        NSComparisonResult result = [aDate compare:distantPath];
+        NSLog(@"isEqual:%d, result:%ld", isEqual, result);
+        //6.earlierDate and laterDate
+        NSDate *earlierDate = [aDate earlierDate:distantPath];
+        NSDate *laterDate = [aDate laterDate:distantFuture];
+        NSLog(@"earlidate:%@, laterDate:%@", earlierDate, laterDate);
+        
+        //7.用户手动改变系统时间的通知
+//        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(systemClockDidChange:) name:NSSystemClockDidChangeNotification object:nil];
+        
     }
     return 0;
 }
--- Foundation/NSMutableDictionary/NSMutableDictionary/main.m
+++ Foundation/NSMutableDictionary/NSMutableDictionary/main.m
@@ -2,15 +2,38 @@
 //  main.m
 //  NSMutableDictionary
 //
-//  Created by pantosoft on 2017/7/17.
+//  Created by pantosoft on 2017/7/18.
 //  Copyright © 2017年 jianghongbing. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-#import "AppDelegate.h"
+#import <Foundation/Foundation.h>
 
-int main(int argc, char * argv[]) {
+int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+        //NSMutableDictionary:可变的NSDictionary
+        NSDictionary *dictionary = @{@"name" : @"jiang", @"age" : @27};
+        NSMutableDictionary *mutableDictionary = [NSMutableDictionary dictionaryWithDictionary:dictionary];
+        //1.修改指定key的值,如果字典中没有对应的key,将就key添加到字典中,并赋予对应的值
+        mutableDictionary[@"name"] = @"hong";
+        NSLog(@"mutableDictionary:%@", mutableDictionary);
+        [mutableDictionary setObject:@28 forKey:@"age"];
+        NSLog(@"mutableDictionary:%@", mutableDictionary);
+        mutableDictionary[@"address"] = @"wuhan";
+        NSLog(@"mutableDictionary:%@", mutableDictionary);
+        
+        //2.移除对应key的值
+        mutableDictionary[@"address"] = nil;
+        NSLog(@"mutableDictionary:%@", mutableDictionary);
+        [mutableDictionary removeObjectForKey:@"age"];
+        NSLog(@"mutableDictionary:%@", mutableDictionary);
+        
+        //3.将另外一字典的元素添加到当前字典,若另外的字典中和当前字典中有相同的key时,会将对应key的值也替换掉
+        [mutableDictionary addEntriesFromDictionary:dictionary];
+        NSLog(@"mutableDictionary:%@", mutableDictionary);
+        
+        //4.移除一组key所对应的值和移除所有的值
+        [mutableDictionary removeObjectsForKeys:@[@"age"]];
+        [mutableDictionary removeAllObjects];
     }
+    return 0;
 }
