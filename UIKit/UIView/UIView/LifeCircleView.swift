4585e4d8f71c94e94cc46d22e7fa6aa988198611
jianghongbing <jianghongbing1989@qq.com>
2017-08-13 17:01:25 +0800

    update

--- UIKit/UIView/UIView/LifeCircleView.swift
+++ UIKit/UIView/UIView/LifeCircleView.swift
@@ -9,13 +9,67 @@
 import UIKit
 
 class LifeCircleView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        print("func:\(#function), line:\(#line)")
     }
-    */
+
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        print("func:\(#function), line:\(#line)")
+    }
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        print("func:\(#function), line:\(#line)")
+    }
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        print("func:\(#function), line:\(#line)")
+    }
+
+    override func draw(_ rect: CGRect) {
+        print("func:\(#function), line:\(#line)")
+    }
+
+    override func updateConstraints() {
+        super.updateConstraints()
+        print("func:\(#function), line:\(#line)")
+    }
+
+    override func willMove(toWindow newWindow: UIWindow?) {
+        print("func:\(#function), line:\(#line)")
+    }
+
+    override func willMove(toSuperview newSuperview: UIView?) {
+        print("func:\(#function), line:\(#line)")
+    }
+    override func didMoveToSuperview() {
+        super.didMoveToSuperview()
+        print("func:\(#function), line:\(#line)")
+    }
+
+
+    override func willRemoveSubview(_ subview: UIView) {
+        print("func:\(#function), line:\(#line)")
+        super.willRemoveSubview(subview)
+    }
+
+    override func removeFromSuperview() {
+        super.removeFromSuperview()
+        print("func:\(#function), line:\(#line)")
+    }
+
+
+
+    deinit {
+        print("func:\(#function), line:\(#line)")
+    }
+
+//    override func prepareForInterfaceBuilder() {
+//        print("func:\(#function), line:\(#line)")
+//    }
 
 }
--- UIKit/UIView/UIView/LifeCircleViewController.swift
+++ UIKit/UIView/UIView/LifeCircleViewController.swift
@@ -10,26 +10,62 @@ import UIKit
 
 class LifeCircleViewController: UIViewController {
 
+    var redView: UIView?
+    @IBOutlet weak var yellowView: LifeCircleView!
+    @IBOutlet weak var yellowViewHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var yellowViewWidthConstraint: NSLayoutConstraint!
     override func viewDidLoad() {
         super.viewDidLoad()
+        navigationController?.setToolbarHidden(false, animated: true)
+        //1.创建view的三种方式
 
-        // Do any additional setup after loading the view.
+        //1.1.使用initWithFrame的方式来创建View:会调用initWithFrame方法,不会调用awakeFromNib和initWithCoder的方法
+        self.initViewByFrame()
+        //1.2.使用nib的形式创建view,会调用awakeFromNib方法,不会调用initWithFrame和initWithCoder方法
+        self.initViewByNib()
+        //1.3.直接在storyboard的controller中添加View,会调用initWithCoder和awakeNib方法,不会调用initWithFrame的方法
+        //2.通过任何一种方式创建view后并且将其添加到superView中,会调用updateConstraints,layoutSubiviews和drawInRect方法
+        //2.1 updateConstraints:用于更新自己和subviews的layoutConstraints
+        //2.2 layoutSubviews:用户更精确的布局subview
+        //2.3 drawInRect:绘制view里面的内容
+        //3.当将一个视图添加到superView中去,也调用,willMoveToSuperView,didMoveToSuperView方法
+        //4.view从superView中移除时,会调用willRemoveSuperView,
+        //5.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func initViewByFrame() {
+        let redView = LifeCircleView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        redView.backgroundColor = .red
+        self.redView = redView
+        view.addSubview(redView)
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    private func initViewByNib() {
+        let blueView = Bundle.main.loadNibNamed("TestView", owner: nil, options: nil)?.last as? UIView
+        blueView?.frame = CGRect(x: 100, y: 250, width: 100, height: 100)
+        if let blueView = blueView {
+            view.addSubview(blueView)
+        }
     }
-    */
+    @IBAction func reUpdateConstraints(_ sender: Any) {
 
+    }
+    @IBAction func reUpdateConstraintsIfNeed(_ sender: Any) {
+
+    }
+    @IBAction func reLayoutSubviews(_ sender: Any) {
+        if let redView = self.redView {
+            redView.setNeedsLayout() //强制重新布局,会调用layoutSubview方法
+        }
+    }
+    @IBAction func reLayoutSubviewsIfNeed(_ sender: Any) {
+        if let redView = self.redView {
+            var frame = redView.frame
+            frame.size = CGSize(width: 200, height: 200)
+            redView.frame = frame
+            redView.layoutIfNeeded() //
+        }
+    }
+    @IBAction func reDraw(_ sender: Any) {
+    }
 }
