502b68caaf22eb8bda6ccb9230a389e0c8377fc8
jianghongbing <jianghongbing1989@qq.com>
2017-08-20 20:45:25 +0800

    update

---
+++ UIKit/UITextView/UITextView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITextView
+//
+//  Created by jianghongbing on 2017/8/20.
+//  Copyright © 2017年 jianghongbing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var topTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //UITextView:用于排版和显示文本
+    }
+}
+
