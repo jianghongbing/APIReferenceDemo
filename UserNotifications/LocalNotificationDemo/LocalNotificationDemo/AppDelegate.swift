583f9b37a669e7940dec11b0c1f7f8cea95071e6
pantosoft <pantosoft@pantosoftdeMac-mini.local>
2017-09-08 17:29:17 +0800

    update

---
+++ UserNotifications/LocalNotificationDemo/LocalNotificationDemo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  LocalNotificationDemo
+//
+//  Created by pantosoft on 2017/9/7.
+//  Copyright © 2017年 jianghongbing. All rights reserved.
+//
+
+import UIKit
+import UserNotifications
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+//        let notificationCenter = UNUserNotificationCenter.current()
+//        notificationCenter.delegate = 
+        return true
+    }
+}
+
---
+++ UserNotifications/LocalNotificationDemo/LocalNotificationDemo/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  LocalNotificationDemo
+//
+//  Created by pantosoft on 2017/9/7.
+//  Copyright © 2017年 jianghongbing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+}
+
