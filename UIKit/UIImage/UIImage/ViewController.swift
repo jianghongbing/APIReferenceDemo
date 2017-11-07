dc839d8de282bdea8f2a06a325cc4faa2c173905
jianghongbing <>
2017-11-07 17:50:35 +0800

    update

---
+++ UIKit/UIImage/UIImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIImage
+//
+//  Created by pantosoft on 2017/11/3.
+//  Copyright © 2017年 jianghongbing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let image = UIImage()
+    }
+}
+
