57ae84126aa5613d2d0b8538a0013f918d84c04e
jianghongbing <>
2018-02-09 13:25:36 +0800

    update

--- Core Animation/CAAnimation/CAAnimation/ViewController.m
+++ Core Animation/CAAnimation/CAAnimation/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+static NSString * const kAnimationKeyPath = @"redViewLayerAnimation";
 @interface ViewController ()<CAAnimationDelegate>
 @property (weak, nonatomic) IBOutlet UIView *redView;
 
@@ -18,31 +18,31 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //CAAnimation:动画的抽象类,用于描述某个动画,layer通过添加CAAnimation来实现动画
-
-    
-    
 }
 #pragma mark CAAnimationDelegate
+//动画开始的时候的回调
 - (void)animationDidStart:(CAAnimation *)anim {
-    
+    NSLog(@"func:%s", __func__);
 }
-
+//动画结束的回调,flag:YES:表示动画完成, NO:表示动画取消
 - (void)animationDidStop:(CAAnimation *)anim finished:(BOOL)flag {
-    
+    NSLog(@"func:%s, flag:%d", __func__, flag);
 }
 
 
 - (IBAction)startAnimation:(id)sender {
-//    CAAnimation *animation = [CABasicAnimation animationWithKeyPath:@"position.x"];
-//    //1.设置动画的代理
-//    animation.delegate = self;
-//    //2.CAMediaTiming:用于管理动画的时间
-//    //
-//    animation.beginTime = 2.0;
-//    animation.duration = 2.0;
-//    animation.speed = 1.5;
-    
+//    [self.redView.layer removeAllAnimations];
+    [self.redView.layer removeAnimationForKey:kAnimationKeyPath];
+    CABasicAnimation *basicAnimation = [CABasicAnimation animation];
+    basicAnimation.keyPath = @"position.y";
+    basicAnimation.fromValue = @(self.redView.layer.position.y);
+    basicAnimation.byValue = @200;
+    basicAnimation.duration = 2.0;
+    basicAnimation.repeatCount = 5.0;
+    basicAnimation.delegate = self;
+    [self.redView.layer addAnimation:basicAnimation forKey:kAnimationKeyPath];
 }
 - (IBAction)removeAnimation:(id)sender {
+    [self.redView.layer removeAnimationForKey:kAnimationKeyPath];
 }
 @end
