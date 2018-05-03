01a016feb135c04fc25b59d40afad64f6f4cd33f
jianghongbing <>
2018-05-03 21:09:29 +0800

    update

---
+++ Foundation/NSComparisonPredicate/NSComparisonPredicate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSComparisonPredicate
+//
+//  Created by pantosoft on 2018/5/3.
+//  Copyright © 2018年 jianghongbing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSComparisonPredicate *predicate = [NSComparisonPredicate predicateWithLeftExpression:nil rightExpression:nil customSelector:nil];
+    }
+    return 0;
+}
---
+++ Foundation/NSExpression/NSExpression/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSExpression
+//
+//  Created by pantosoft on 2018/5/3.
+//  Copyright © 2018年 jianghongbing. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
