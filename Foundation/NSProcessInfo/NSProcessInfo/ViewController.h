012bf24eca01d3dfa21f5fe3e579f14a820a40a8
jianghongbing <>
2018-05-30 18:13:50 +0800

    update

---
+++ Foundation/NSProcessInfo/NSProcessInfo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  NSProcessInfo
+//
+//  Created by pantosoft on 2018/5/30.
+//  Copyright © 2018年 jianghongbing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Foundation/NSProcessInfo/NSProcessInfo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSProcessInfo
+//
+//  Created by pantosoft on 2018/5/30.
+//  Copyright © 2018年 jianghongbing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
